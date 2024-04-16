1833998b8bde743049a02c7c92a495ba07631750
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-04-16 11:13:49 +0100

    Update appshell

--- src/components/shells/appshell.js
+++ src/components/shells/appshell.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../main/header";
 import Footer from "../main/footer";
+import { Outlet } from "react-router-dom";
 
 const AppShell = () => {
   return (
     <>
       <Header />
+      <Outlet />
       <Footer />
     </>
   );
