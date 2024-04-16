4941622365e170c1002a9c4f999dfd281162f17d
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-04-16 11:13:14 +0100

    Update app routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppShell from "./components/shells/appshell";
+import Api from "./pages/Api";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <AppShell />,
+      children: [
+        {
+          path: "apis",
+          element: <Api />,
+        },
+      ],
     },
   ]);
 
