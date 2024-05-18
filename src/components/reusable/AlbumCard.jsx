1a4513f66f7c2309343a058ca2841e161f1cef50
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-05-18 16:14:37 +0100

    Change widths of albums

--- src/components/reusable/AlbumCard.jsx
+++ src/components/reusable/AlbumCard.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const AlbumCard = ({ img, title, artist, link, dateReleased }) => {
   return (
-    <div className=" w-[90%] lg:w-[400px] rounded-lg m-1 overflow-hidden cursor-pointer shadow-xl transition-all ease-linear transition-200ms hover:scale-105 hover:shadow-2xl p-4">
+    <div className=" w-[90%] lg:w-80 rounded-lg m-1 overflow-hidden cursor-pointer shadow-xl transition-all ease-linear transition-200ms hover:scale-105 hover:shadow-2xl p-4">
       <a href={link}>
         <img src={img} alt={title} className="w-full object-cover" />
-        <ul className=" font-merriweatherBold my-4 leading-7">
+        <ul className=" font-merriweatherBold my-4 leading-7 flex flex-col gap-4">
           <li>
             Artist: <span className="font-merriweatherReg">{artist}</span>
           </li>
