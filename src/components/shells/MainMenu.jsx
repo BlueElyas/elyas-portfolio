14a3a63a85bca5c5834f832991453264b9cf85cf
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-05-18 15:55:34 +0100

    Added opener to the front page

--- src/components/shells/MainMenu.jsx
+++ src/components/shells/MainMenu.jsx
@@ -7,10 +7,16 @@ const MainMenu = () => {
   return (
     <div className="flex justify-center items-center flex-col bg-darkGray text-softGray h-screen gap-8 text-lg lg:text-4xl font-merriweatherBold text-center overflow-hidden">
       <MenuTitle />
-      <p className="text-lg font-sans mx-8 w-96">
+      <p className="text-lg font-sans mx-8 w-1/3">
         My name is{" "}
         <span class="text-xl font-semibold text-orange-500">Elyas</span>
-        <p className="block my-4">Welcome to my page 👋 </p>
+        <p className="block my-4 text-orange-500">Welcome to my page 👋 </p>
+        <p class="font-normal text-md md:text-xl">
+          Front-End Developer skilled in HTML/CSS, JavaScript, React, and
+          TypeScript, with a focus on creating responsive and accessible web
+          applications. Confidence in utilizing Typescript, HTML/CSS, and React
+          to deliver high-quality projects
+        </p>
       </p>
       <MainMenuButton name="View CV" link="cv.html" />
       <MainMenuButton name="View Portfolio" link="personal" />
