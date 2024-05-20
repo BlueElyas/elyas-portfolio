28d3238574ea7cf5ab0269284436b82337628d36
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-05-20 22:01:45 +0100

    Hide the header

--- src/components/reusable/PageTitle.jsx
+++ src/components/reusable/PageTitle.jsx
@@ -34,7 +34,7 @@ const PageTitle = ({ title }) => {
   return (
     <div className="pt-6 pb-10 text-center font-merriweatherBold px-8 my-4 leading-8 grid grid-cols-3 grid-rows-2">
       {showBackToHome}
-      <h1 className="text-3xl mt-6 col-span-full"> {title} </h1>
+      <h1 className="text-3xl mt-6 md:mt-0 col-span-full"> {title} </h1>
     </div>
   );
 };
--- src/components/shells/appshell.jsx
+++ src/components/shells/appshell.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import Header from "../main/header";
 import Footer from "../main/footer";
 import { Outlet } from "react-router-dom";
 
 const AppShell = () => {
   return (
-    <React.Fragment>
-      <Header />
+    <div className="bg-softGray pt-1">
+      {/* <Header /> */}
       <Outlet />
       <Footer />
-    </React.Fragment>
+    </div>
   );
 };
 
