e3abdc5a46d96ebf43a4d442f633bc4bad00ac18
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-06-08 13:57:52 +0100

    Updated styles

--- src/components/reusable/AlbumCard.jsx
+++ src/components/reusable/AlbumCard.jsx
@@ -4,36 +4,34 @@ import { NavLink } from "react-router-dom";
 const AlbumCard = ({ project }) => {
   const { name, description, images, slug } = project;
   return (
-    <NavLink to={slug}>
-      <div className=" lg:w-80 rounded-lg m-1 overflow-hidden cursor-pointer shadow-xl transition-all ease-linear transition-200ms hover:shadow-2xl p-4 bg-white text-darkGray text-start ">
-        <img src={images[0]} alt={name} className="w-full object-cover " />
-        <ul className="  my-4 leading-7 flex flex-col gap-4">
-          <li className="font-merriweatherBold">{name}</li>
-          <li>{description}</li>
-          <NavLink
-            to={slug}
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none justify-center bg-blue-600 "
+    <div className=" rounded-lg m-1 overflow-hidden cursor-pointer transition-all ease-linear transition-200ms hover:shadow-2xl p-4 bg-white text-darkGray text-start flex flex-col lg:flex-row-reverse lg:w-1/4 lg:gap-16 lg:p-6">
+      <img src={images[0]} alt={name} className="lg:block lg:w-1/2" />
+      <ul className="  my-4 leading-7 flex flex-col gap-4 lg:justify-center">
+        <li className="font-merriweatherBold">{name}</li>
+        <li>{description}</li>
+        <NavLink
+          to={slug}
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none justify-center bg-blue-600 "
+        >
+          Check it out!
+          <svg
+            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
           >
-            Check it out!
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </NavLink>
-        </ul>
-      </div>
-    </NavLink>
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </NavLink>
+      </ul>
+    </div>
   );
 };
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -13,18 +13,20 @@ const Projects = () => {
 
   return (
     <main>
-      <section className="text-center w-[80%] mx-auto">
-        <PageTitle title="Portfolio" />
-        <div className="leading-8">
-          <a
-            href="https://github.com/BlueElyas"
-            className="underline hover:no-underline hover:opacity-75 text-xl active:opacity-30"
-            target="_noblank"
-          >
-            Visit my github here!
-          </a>
+      <section className="text-center mx-auto">
+        <div className="text-center w-[80%] mx-auto">
+          <PageTitle title="Portfolio" />
+          <div className="leading-8">
+            <a
+              href="https://github.com/BlueElyas"
+              className="underline hover:no-underline hover:opacity-75 text-xl active:opacity-30 mb-16"
+              target="_noblank"
+            >
+              Visit my github here!
+            </a>
+          </div>
         </div>
-        <div className="mt-8 grid gap-16 md:gap-32 md:grid-cols-2 lg:grid-cols-3 items-start">
+        <div className="flex flex-col justify-center lg:flex-row gap-8 lg:gap-12 lg:flex-wrap">
           {projects.map((project) => {
             return <AlbumCard project={project} key={project.slug} />;
           })}
